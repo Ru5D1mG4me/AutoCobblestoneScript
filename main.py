028d6cca191d75b26bf726e14447d3ecca33aa85
Ru5D1mG4me <46672460+Ru5D1mG4me@users.noreply.github.com>
2021-08-10 17:32:55 +0500

    fixes

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import pydirectinput
 import keyboard
 
 if __name__ == '__main__':
-    while True:
-        if keyboard.is_pressed("="):
-            print("[AutoCobblestoneScript] Enabled!")
-            while True:
-                if not keyboard.is_pressed("-"):
-                    pydirectinput.keyDown("w")
-                    pydirectinput.mouseDown(button="left")
-                else:
-                    print("[AutoCobblestoneScript] Disabled!")
-                    pydirectinput.keyUp("w")
-                    pydirectinput.mouseUp(button="left")
-                    break
+    try:
+        while True:
+            if keyboard.is_pressed("="):
+                print("[AutoCobblestoneScript] Enabled!")
+                while True:
+                    if not keyboard.is_pressed("-"):
+                        pydirectinput.keyDown("w")
+                        pydirectinput.mouseDown(button="left")
+                    else:
+                        print("[AutoCobblestoneScript] Disabled!")
+                        pydirectinput.keyUp("w")
+                        pydirectinput.mouseUp(button="left")
+                        break
+    except KeyboardInterrupt:
+        input("[AutoCobblestoneScript] Press enter to continue...")
