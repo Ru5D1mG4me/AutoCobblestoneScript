02e39963ef9445632b886d4b80ec99654294eb57
Ru5D1mG4me <46672460+Ru5D1mG4me@users.noreply.github.com>
2021-08-10 13:13:55 +0500

    adding script

---
+++ main.py
@@ -0,0 +1,16 @@
+import pydirectinput
+import keyboard
+
+if __name__ == '__main__':
+    while True:
+        if keyboard.is_pressed("="):
+            print("[AutoCobblestoneScript] Enabled!")
+            while True:
+                if not keyboard.is_pressed("-"):
+                    pydirectinput.keyDown("w")
+                    pydirectinput.mouseDown(button="left")
+                else:
+                    print("[AutoCobblestoneScript] Disabled!")
+                    pydirectinput.keyUp("w")
+                    pydirectinput.mouseUp(button="left")
+                    break
